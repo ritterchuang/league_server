d30b72109847006c0a5d6b2e7e733d835dc21ded
ritter <recluseyouth@gmail.com>
2023-05-27 20:13:36 +0800

    feat: 新增假資料

--- slot-game-server/src/main/java/org/lsj/websocket/SlotWebSocketServer.java
+++ slot-game-server/src/main/java/org/lsj/websocket/SlotWebSocketServer.java
@@ -1,10 +1,16 @@
 package org.lsj.websocket;
 
+import org.lsj.math.entity.CmdOut_NgSpin;
+import org.lsj.math.entity.LinePassReelIndex;
+import org.lsj.util.JsonUtil;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 @ServerEndpoint(value = "/")
 @ApplicationScoped
@@ -34,10 +40,13 @@ public class SlotWebSocketServer {
         String message = new String(messageBytes.array(), StandardCharsets.UTF_8);
 //        LOG.debug("{} message: {}", LogUtil.getLogPrefix(session, 0), message);
 
+        // 1. 打印輸入參數
         System.out.println("received message: " + message);
 
-        String response = "hello " + message;
-        ByteBuffer messageByteBuffer = ByteBuffer.wrap(response.getBytes(StandardCharsets.UTF_8));
+        // 2. 生成假資料(無視輸入)
+        String fakeDataString = this.generateFakeDataString();
+        ByteBuffer messageByteBuffer = this.stringToByteBuffer(fakeDataString);
+        System.out.println("send message: " + fakeDataString);
         try {
             session.getBasicRemote().sendBinary(messageByteBuffer);
 //            if (session.isOpen()) {
@@ -51,4 +60,31 @@ public class SlotWebSocketServer {
         }
     }
 
+    // 字串 轉 字節緩衝區
+    private ByteBuffer stringToByteBuffer(String string){
+        return ByteBuffer.wrap(string.getBytes(StandardCharsets.UTF_8));
+    }
+
+    // 生成假資料
+    private String generateFakeDataString(){
+        CmdOut_NgSpin cmdOut_ngSpin = new CmdOut_NgSpin(
+                new ArrayList<>(){{
+                    add(1);add(1);add(1);add(1);add(1);
+                    add(2);add(2);add(2);add(2);add(2);
+                    add(3);add(3);add(3);add(3);add(3);
+                }},
+                new ArrayList<>(){{
+                    add(new LinePassReelIndex(
+                            new ArrayList<>(){{add(2);add(2);add(2);add(2);add(2);}},
+                            2,
+                            2,
+                            20
+                    ));
+                }},
+                20,
+                3,
+                5
+        );
+        return JsonUtil.getInstance().writeValueAsStringWithoutException(cmdOut_ngSpin);
+    }
 }
